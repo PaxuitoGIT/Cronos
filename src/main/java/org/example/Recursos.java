ebb835e9a0285e20d329d79de4e8a9481742856f
PaxuitoGIT <bxu@myuax.com>
2024-02-24 00:50:40 +0100

    Declarar Recursos

---
+++ src/main/java/org/example/Recursos.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Recursos {
+    private double agua;
+    private double comida;
+    private double oxigeno;
+
+    public Recursos(double agua, double comida, double oxigeno) {
+        this.agua = agua;
+        this.comida = comida;
+        this.oxigeno = oxigeno;
+    }
+}
