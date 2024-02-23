e2bbc573b6646405546c158d1699860b92d58102
PaxuitoGIT <bxu@myuax.com>
2024-02-23 23:39:31 +0100

    Factor de conversion, getters y setters

--- src/main/java/org/example/CronometroCosmico.java
+++ src/main/java/org/example/CronometroCosmico.java
@@ -4,4 +4,35 @@ import java.util.Date;
 public class CronometroCosmico {
     Date fechaPlanetaTierra;
     Date fechaPlanetaNuevo;
+
+    public CronometroCosmico() {
+        fechaPlanetaTierra = new Date();
+        fechaPlanetaNuevo = new Date();
+    }
+
+    static double FC = 0.5;
+
+    public static double getFC() {
+        return FC;
+    }
+
+    public static void setFC(double FC) {
+        CronometroCosmico.FC = FC;
+    }
+
+    public Date getFechaPlanetaTierra() {
+        return fechaPlanetaTierra;
+    }
+
+    public void setFechaPlanetaTierra(Date fechaPlanetaTierra) {
+        this.fechaPlanetaTierra = fechaPlanetaTierra;
+    }
+
+    public Date getFechaPlanetaNuevo() {
+        return fechaPlanetaNuevo;
+    }
+
+    public void setFechaPlanetaNuevo(Date fechaPlanetaNuevo) {
+        this.fechaPlanetaNuevo = fechaPlanetaNuevo;
+    }
 }
