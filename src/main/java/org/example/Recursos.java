ece2efe0a1ebdbb926c9cb8d1a1b9f56924f68cf
PaxuitoGIT <bxu@myuax.com>
2024-02-25 14:12:51 +0100

    Calcular media,minimo y maximo

--- src/main/java/org/example/Recursos.java
+++ src/main/java/org/example/Recursos.java
@@ -10,4 +10,32 @@ public class Recursos {
         this.comida = comida;
         this.oxigeno = oxigeno;
     }
+
+    public double calcularMedia(double[] consumos) {
+        double sum = 0.0;
+        for (double consumo : consumos) {
+            sum += consumo;
+        }
+        return sum / consumos.length;
+    }
+
+    public double calcularMinimo(double[] consumos) {
+        double min = Double.MAX_VALUE;
+        for (double consumo : consumos) {
+            if (consumo < min) {
+                min = consumo;
+            }
+        }
+        return min;
+    }
+
+    public double calcularMaximo(double[] consumos) {
+        double max = Double.MIN_VALUE;
+        for (double consumo : consumos) {
+            if (consumo > max) {
+                max = consumo;
+            }
+        }
+        return max;
+    }
 }
