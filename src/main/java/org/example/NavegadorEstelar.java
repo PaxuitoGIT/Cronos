98fd5e5621bd20d287c4d517c422373c4fdb237a
PaxuitoGIT <bxu@myuax.com>
2024-02-25 17:45:18 +0100

    planificar ruta

--- src/main/java/org/example/NavegadorEstelar.java
+++ src/main/java/org/example/NavegadorEstelar.java
@@ -22,5 +22,28 @@ public class NavegadorEstelar {
             System.out.println("Los puntos de inicio o final están fuera de los límites del terreno");
             return;
         }
+
+        if (startX == endX && startY == endY) {
+            System.out.println("El punto de inicio y final son el mismo");
+            return;
+        }
     }
+
+    int dirX = endX > startX ? 1 : -1;
+    int dirY = endY > startY ? 1 : -1;
+
+    int x = startX;
+    int y = startY;
+    while (x != endX || y != endY) {
+        terreno[x][y] = 2;
+
+        if (x != endX) {
+            x += dirX;
+        }
+        if (y != endY) {
+            y += dirY;
+        }
+    }
+
+    terreno[endX][endY] = 2;
 }
