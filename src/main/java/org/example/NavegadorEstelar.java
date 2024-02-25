26dd0bb1f588086f4beeb8195e50586e0db997fd
PaxuitoGIT <bxu@myuax.com>
2024-02-25 17:42:23 +0100

    Visualizar y planificar ruta

---
+++ src/main/java/org/example/NavegadorEstelar.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class NavegadorEstelar {
+
+    private int[][] terreno;
+
+    public NavegadorEstelar(int[][] terreno) {
+        this.terreno = new int[size][size];
+    }
+
+    public void visualizarTerreno() {
+        for (int i = 0; i < terreno.length; i++) {
+            for (int j = 0; j < terreno[i].length; j++) {
+                System.out.print(terreno[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void planificarRuta(int startX, int startY, int endX, int endY) {
+        if (startX < 0 || startY < 0 || endX >= terreno.length || endY >= terreno[0].length) {
+            System.out.println("Los puntos de inicio o final están fuera de los límites del terreno");
+            return;
+        }
+    }
+}
